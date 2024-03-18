c2c1e748c84fb10ceff8be190adcfc8076c0f367
wlsn0105 <jack13579@naver.com>
2024-03-19 01:20:12 +0900

    Feat

---
+++ 2024_03_19/webanimation/app.js
@@ -0,0 +1,25 @@
+window.onscroll = () => {
+    const largeBox = document.querySelector('.large-box');
+    const viewportHeight = window.innerHeight;
+    // window.innerHeight은 브라우저 창의 뷰포트의 높이를 나타냄
+
+    const viewportToParentHeight = largeBox.getBoundingClientRect().top;
+    // getBoundingClientRect() 메서드는 해당 요소의 위치 및 크기에 대한 정보를 제공함
+    // top 속성은 요소의 상단부터 뷰포트의 맨 위까지의 거리를 나타냄
+
+    const scrollHeight = viewportHeight - viewportToParentHeight;
+    // viewportToParentHeight가 viewportHeight가 0이 되면 시작이다.
+
+    const divHeight = largeBox.clientHeight;
+    // largeBox의 높이를 가져와서 변수에 저장
+
+    const scrollRate = scrollHeight / divHeight * 100;
+    
+    if (scrollHeight / largeBox * 100 < 0) {
+        scrollRate = 0;
+    }
+    else if (largeBox / divHeight * 100 > 100) {
+        scrollRate = 100;
+    }
+    console.log(scrollRate);
+}
