8b3e7ce5551662129b05617357d8630e2fcbabb6
wlsn0105 <jack13579@naver.com>
2024-06-05 16:29:57 +0900

    Feat: 몽고DB 사용

--- 2024_06/06_05/#2/boiler-plate/index.js
+++ 2024_06/06_05/#2/boiler-plate/index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const mongoose = require("mongoose");
+mongoose
+    .connect(
+        "mongodb+srv://zinu:9692@nodereacttolearn.so8ghj9.mongodb.net/?retryWrites=true&w=majority&appName=NodeReactToLearn",
+        {}
+    )
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
